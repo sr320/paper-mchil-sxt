112be17a79a648d55b16ffc418740f87438a59ba
Steven Roberts <sr320@uw.edu>
2015-12-04 16:05:58 -0300

    added trinity assembly
    This is from reads untrimmed

---
+++ data/run.All.kmer2.untrimmed.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+KMER=2
+
+LD_LIBRARY_PATH=/share/bioinformatics/gcc/lib64:/share/bioinformatics/gcc/lib
+
+TRIN=/data/ggoetz/trinityrnaseq-2.0.6
+SAMTOOLS=/share/bioinformatics/samtools/bin
+BOWTIE=/share/bioinformatics/bowtie
+
+PATH=${SAMTOOLS}:${BOWTIE}:${PATH}
+
+${TRIN}/Trinity \
+	--seqType fq \
+	--max_memory 400G \
+	--left mchil-sxt_1.fastq \
+	--right mchil-sxt_2.fastq \
+	--CPU 8 \
+	--bflyCalculateCPU \
+	--bflyHeapSpaceMax 16G \
+	--bflyHeapSpaceInit 8G \
+	--normalize_reads \
+	--min_kmer_cov ${KMER} \
+	--output trinity_All.kmer${KMER} \
+	> trinity_output.All.kmer${KMER}.log
